323473304d5e68d027b24a4f8729d875470a5e39
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-02-15 11:45:16 -0800

    start up!

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import "@fontsource/rock-salt";
+import "@fontsource/yeseva-one";
+import "@fontsource/poppins";
+import "@fontsource/shadows-into-light";
+
+
+function App() {
+    return (
+        <>
+        <h1>hello!</h1>
+        </>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('react-container'));
