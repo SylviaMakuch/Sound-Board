a982c5a3d91a983ff49778e1b2f12145d7d8a44c
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-02-16 23:35:30 -0800

    Beat- config file

--- App.tsx
+++ App.tsx
@@ -4,14 +4,12 @@ import "@fontsource/rock-salt";
 import "@fontsource/yeseva-one";
 import "@fontsource/poppins";
 import "@fontsource/shadows-into-light";
-import AudioPlayer from "./components/audioplayer"
 import { AudioPlayerProvider } from "react-use-audio-player"
-import bouncybass from "./sounds/bouncybass.mp3";
+
 
 const App = () => {
     return (
         <AudioPlayerProvider>
-            <AudioPlayer file={bouncybass} />
         </AudioPlayerProvider>
     )
 }
---
+++ components/config.tsx
@@ -0,0 +1,44 @@
+import beatloop from "./../sounds/beats/beatloop.mp3";
+import breakbassloop from "./../sounds/beats/breakbassloop.mp3";
+import dnbloop from "./../sounds/beats/dnbloop.mp3";
+import funkybeats from "./../sounds/beats/funkybeats.mp3";
+import heavybreaker from "./../sounds/beats/heavybreaker.mp3";
+import hiphopbeat from "./../sounds/beats/hiphopbeat.mp3";
+import megadrumbeat from "./../sounds/beats/megadrumbeat.mp3";
+import puertoricobeat from "./../sounds/beats/puertoricobeat.mp3";
+
+export const Beatsdata = [
+    {
+        name: '140 bpm Beat',
+        src: beatloop
+    },
+    {
+        name: 'Break Bass ',
+        src: breakbassloop
+    },
+    {
+        name: ' DnB Beat Loop',
+        src: dnbloop
+    },
+    {
+        name: 'Funky Beat',
+        src: funkybeats
+
+    },
+    {
+        name: 'Heavt Beat',
+        src: heavybreaker
+    },
+    {
+        name: 'Hip-Hop Beat',
+        src: hiphopbeat
+    },
+    {
+        name: 'Mega Drum Beat',
+        src: megadrumbeat
+    },
+    {
+        name: 'Pureto Rico Beat',
+        src: puertoricobeat
+    },
+];
